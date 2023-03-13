e1e24ba6ba7d20987a8d300a433200e62f4aac36
Rodolphe Terlinden <rodolpheterlinden@gmail.com>
2023-03-13 16:16:55 -0300

    list packages names and dependencies

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(name='hungry_birds',
+      version="0.0.1",
+      description="Xeno Canto Project",
+      install_requires=requirements,
+      packages=find_packages())
