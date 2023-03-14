c20f23d1f37a61ca3f7fd053eab9563b784c3d9f
Rodolphe Terlinden <rodolpheterlinden@gmail.com>
2023-03-14 11:43:49 -0300

    api

--- api/fast.py
+++ api/fast.py
@@ -7,14 +7,13 @@ import io
 
 app = FastAPI()
 
-# # Allow all requests (optional, good for development purposes)
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  # Allows all origins
-#     allow_credentials=True,
-#     allow_methods=["*"],  # Allows all methods
-#     allow_headers=["*"],  # Allows all headers
-# )
+#@app.post("/predict/")
+#async def predict_audio(file: UploadFile = File(...)):
+    # Process the audio file
+    # ...
+    # Return the predicted class label
+    #return {"class": "music"}
+
 
 @app.get("/")
 def index():
