cd625c14ecee62a44966bf33209a5f5e21048dfb
Rodolphe Terlinden <rodolpheterlinden@gmail.com>
2023-03-15 16:50:40 -0300

    modifs on streamlit

--- api/streamlit.py
+++ api/streamlit.py
@@ -2,7 +2,24 @@ import streamlit as st
 
 import numpy as np
 import pandas as pd
+import requests
+
+url = "http://localhost:8000/"
+
+with st.form(key='params_for_api'):
+    your_path_to_wav_file = st.number_input('Your path to wav file', value="/Users/.../...wav")
+
+    st.form_submit_button('Make prediction')
+
+params = your_path_to_wav_file
+
+st.set_page_config(
+    page_title="Hungry Birds",
+    page_icon="🦤",
+)
 
 st.markdown("""# Hungry birds Tool
 ## Which bird specy is singing in your garden?
 Import your .wav file""")
+
+response = requests.get(url, params=params)
