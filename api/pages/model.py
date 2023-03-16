08220aafc0f64ed429dcb382a0b87b3b2a4b2e7b
Rodolphe Terlinden <rodolpheterlinden@gmail.com>
2023-03-16 15:12:28 -0300

    test

--- api/pages/model.py
+++ api/pages/model.py
@@ -49,52 +49,50 @@ upload_file = st.file_uploader("Choose an audio file", type=[".wav"], accept_mul
 if upload_file is not None:
     audio_bytes = upload_file.read()
     st.audio(audio_bytes,format="audio/wav")
+    if st.button("Make prediction"):
+        #importing the model
+        yamnet_model = hub.load('https://tfhub.dev/google/yamnet/1')
+        print(os.getcwd())
+        my_model = tf.keras.models.load_model("yamnet_full", compile=False)
+        # my_model=tf.saved_model.load(
+        #            "/home/tcosendey/code/Tfcosendey/hungry_birds/yamnet_full")
+        assert my_model is not None
 
+        # preprocessing
+        def load_wav_16k_mono(filename):
+            """ Load a WAV file, convert it to a float tensor, resample to 16 kHz single-channel audio. """
+            wav, sample_rate = librosa.load(filename, sr=16000, mono=True)
+            wav = tf.convert_to_tensor(wav, dtype=tf.float32)
+            return wav
 
-#importing the model
-yamnet_model = hub.load('https://tfhub.dev/google/yamnet/1')
-print(os.getcwd())
-my_model = tf.keras.models.load_model("yamnet_full", compile=False)
-# my_model=tf.saved_model.load(
-#            "/home/tcosendey/code/Tfcosendey/hungry_birds/yamnet_full")
-assert my_model is not None
+        # embedding
 
-# preprocessing
-def load_wav_16k_mono(filename):
-    """ Load a WAV file, convert it to a float tensor, resample to 16 kHz single-channel audio. """
-    wav, sample_rate = librosa.load(filename, sr=16000, mono=True)
-    wav = tf.convert_to_tensor(wav, dtype=tf.float32)
-    return wav
+        def extract_embedding(wav_data, label):
+            scores, embeddings, spectrogram = yamnet_model(wav_data)
+            scores = tf.reshape(scores[:,106], [-1, 1])
+            embeddings =  scores * embeddings
+            num_embeddings = tf.shape(embeddings)[0]
+            return embeddings, tf.repeat(label, num_embeddings)
 
-# embedding
+        #the model
 
-def extract_embedding(wav_data, label):
-  ''' run YAMNet to extract embedding from the wav data '''
-  scores, embeddings, spectrogram = yamnet_model(wav_data)
-  scores = tf.reshape(scores[:,106], [-1, 1])
-  embeddings =  scores * embeddings
-  num_embeddings = tf.shape(embeddings)[0]
-  return embeddings, tf.repeat(label, num_embeddings)
+        filename = "/home/tcosendey/code/Tfcosendey/hungry_birds/Drymophila ochropyga.wav"
 
-#the model
-
-filename = "/home/tcosendey/code/Tfcosendey/hungry_birds/Drymophila ochropyga.wav"
-
-def predict(upload_file):
-    wav = load_wav_16k_mono(upload_file)
-    # yamnet model
-    scores, embeddings, spectrogram = yamnet_model(wav)
-    scores = scores[:,106]
-    scores = tf.reshape(scores, [-1, 1])
-    final_scores = scores * my_model(embeddings)
-    final_scores = tf.reduce_sum(final_scores, axis=0)
-    row_sum = tf.reduce_sum(final_scores)
-    final_scores = tf.divide(final_scores, row_sum)
-    final_score = pd.DataFrame(final_scores, columns = ['Probability'])
-    with open('yamnet_full/label_encoder.pkl', 'rb') as f:
-        le = pickle.load(f)
-    final_score.index = le.inverse_transform(final_score.index)
-    final_score = final_score.sort_values(by = 'Probability', ascending = False).applymap(lambda x: "{:.2%}".format(x))
-    print(final_score.head(10).index)
-    os.remove(upload_file)
-    return final_score.head(10).index
+        def predict(upload_file):
+            wav = load_wav_16k_mono(upload_file)
+            # yamnet model
+            scores, embeddings, spectrogram = yamnet_model(wav)
+            scores = scores[:,106]
+            scores = tf.reshape(scores, [-1, 1])
+            final_scores = scores * my_model(embeddings)
+            final_scores = tf.reduce_sum(final_scores, axis=0)
+            row_sum = tf.reduce_sum(final_scores)
+            final_scores = tf.divide(final_scores, row_sum)
+            final_score = pd.DataFrame(final_scores, columns = ['Probability'])
+            with open('yamnet_full/label_encoder.pkl', 'rb') as f:
+                le = pickle.load(f)
+            final_score.index = le.inverse_transform(final_score.index)
+            final_score = final_score.sort_values(by = 'Probability', ascending = False).applymap(lambda x: "{:.2%}".format(x))
+            print(final_score.head(10).index)
+            os.remove(upload_file)
+            return final_score.head(10).index
