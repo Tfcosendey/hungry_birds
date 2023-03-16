ec747841bec7bfbb908c326d1f895e0774c1f157
Rodolphe Terlinden <rodolpheterlinden@gmail.com>
2023-03-16 10:02:45 -0300

    streamlit

--- api/streamlit.py
+++ api/streamlit.py
@@ -12,20 +12,15 @@ st.set_page_config(
     page_icon="🦤")
 
 st.markdown("""# Hungry birds Tool
-## Which bird specy is singing in your garden?
-Import your .wav file""")
+## Which bird specy is singing in your garden?""")
 
 upload_file = st.file_uploader("Choose an audio file", type=[".wav"], accept_multiple_files=False)
 
 if upload_file is not None:
     audio_bytes = upload_file.read()
     st.audio(audio_bytes,format="audio/wav")
-
-with st.form(key='params_for_api'):
-    your_path_to_wav_file = st.number_input('Your path to wav file', value=45)
-
     st.form_submit_button('Make prediction')
 
-params = your_path_to_wav_file
+params = upload_file
 
 response = requests.get(url, params=params)
