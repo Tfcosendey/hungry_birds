54809c619a37c408f32e6c04fcf5f93dcc4d0bc8
Rodolphe Terlinden <rodolpheterlinden@gmail.com>
2023-03-16 11:41:20 -0300

    adding pages

--- api/pages/About1.py
+++ api/pages/About1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 
 st.set_page_config(
-    page_title="About",
+    page_title="About1",
     page_icon="🦤",
     layout="wide",
     initial_sidebar_state="expanded")
