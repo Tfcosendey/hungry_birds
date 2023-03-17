cba9e1ddd92ca8a9713b8f745abe529614ca3da6
Rodolphe Terlinden <rodolpheterlinden@gmail.com>
2023-03-17 11:02:18 -0300

    audiogram

--- api/pages/Audio_file.py
+++ api/pages/Audio_file.py
@@ -20,6 +20,7 @@ from pydub import AudioSegment
 from scipy.io import wavfile
 from tempfile import mktemp
 import os
+import io
 
 import numpy as np
 import pandas as pd
@@ -41,20 +42,73 @@ st.markdown("""# Hungry Birds Project
 # Upload file
 audio_path = st.file_uploader("Choose an audio file", type=[".wav"], accept_multiple_files=False)
 
+#audiogram
+if audio_path is not None:
+    audio_bytes = audio_path.read()
+    st.audio(audio_bytes, format="audio/wav")
+    if st.button("Plot an audiogram"):
+        def audiogram(audio_data):
+            with open('temp_wav_file.wav', 'wb') as wav_file:
+                wav_file.write(audio_data)
+            y, sr = librosa.load('temp_wav_file.wav')
 
-# Draft the audiogram etc
-with audio_path as wav_file:
-    if audio_path is not None:
-        audio_bytes = audio_path.read()
-        st.audio(audio_bytes,format="audio/wav")
+            time = librosa.times_like(y)
+            fig, ax = plt.subplots(figsize=(14, 5))
+
+            ax.plot(time, y)
+            ax.set_xlabel('Time (seconds)')
+            ax.set_ylabel('Amplitude')
+            ax.set_title('Audio waveform')
+            st.pyplot(fig)
+
+        audiogram(audio_bytes)
+
+#spectogram
+if audio_path is not None:
+    audio_bytes = audio_path.read()
+    if st.button("Plot a spectrogram"):
+        def plot_spectrogram(audio_bytes):
+            with open('temp_wav_file.wav', 'wb') as wav_file:
+                wav_file.write(audio_bytes)
+            audio_data, sample_rate = librosa.load('temp_wav_file.wav')
+
+            S = librosa.feature.melspectrogram(y=audio_data, sr=sample_rate, n_mels=128, fmax=8000)
+            S_DB = librosa.power_to_db(S, ref=np.max)
+
+            fig, ax = plt.subplots(figsize=(14, 5))
+            img = librosa.display.specshow(S_DB, x_axis='time', y_axis='mel', sr=sample_rate, ax=ax, fmax=8000)
+            fig.colorbar(img, ax=ax, format='%+2.0f dB')
+            ax.set(title='Mel-frequency spectrogram')
+            st.pyplot(fig)
+
+        plot_spectrogram(audio_bytes)
 
 
-    y, sr = librosa.load(audio_path)
-    time = librosa.times_like(y)
-    fig, ax = plt.subplots(figsize=(14, 5))
+def plot_spectrogram(audio_bytes):
+    with io.BytesIO(audio_bytes) as audio_file:
+        # Load audio data
+        audio_data, sample_rate = librosa.load(audio_file)
 
-    ax.plot(time, y)
-    ax.set_xlabel('Time (seconds)')
-    ax.set_ylabel('Amplitude')
-    ax.set_title('Audio waveform')
-    st.pyplot(fig)
+        # Save audio data to disk
+        with open('temp_wav_file.wav', 'wb') as wav_file:
+            librosa.output.write_wav(wav_file, audio_data, sample_rate)
+
+        # Load saved audio data
+        audio_data, sample_rate = librosa.load('temp_wav_file.wav')
+
+        # Compute spectrogram
+        S = librosa.feature.melspectrogram(y=audio_data, sr=sample_rate, n_mels=128, fmax=8000)
+        S_DB = librosa.power_to_db(S, ref=np.max)
+
+        # Plot spectrogram
+        fig, ax = plt.subplots(figsize=(14, 5))
+        img = librosa.display.specshow(S_DB, x_axis='time', y_axis='mel', sr=sample_rate, ax=ax, fmax=8000)
+        fig.colorbar(img, ax=ax, format='%+2.0f dB')
+        ax.set(title='Mel-frequency spectrogram')
+        st.pyplot(fig)
+
+if audio_path is not None:
+    audio_bytes = audio_path.read()
+    st.audio(audio_bytes, format="audio/wav")
+    if st.button("Plot a spectrogram"):
+        plot_spectrogram(audio_bytes)
